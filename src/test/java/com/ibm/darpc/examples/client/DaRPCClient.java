04e1ce7c89d1426e3146110008e5e714005c9027
Adrian Schuepbach <dri@zurich.ibm.com>
2017-09-20 15:57:38 +0200

    Add memory pool and register memory regions only once. Allow independent send/receive queue lengths.

--- src/test/java/com/ibm/darpc/examples/client/DaRPCClient.java
+++ src/test/java/com/ibm/darpc/examples/client/DaRPCClient.java
@@ -34,6 +34,7 @@ import com.ibm.darpc.DaRPCClientEndpoint;
 import com.ibm.darpc.DaRPCClientGroup;
 import com.ibm.darpc.DaRPCEndpoint;
 import com.ibm.darpc.DaRPCFuture;
+import com.ibm.darpc.DaRPCMemPool;
 import com.ibm.darpc.DaRPCStream;
 import com.ibm.darpc.examples.protocol.RdmaRpcProtocol;
 import com.ibm.darpc.examples.protocol.RdmaRpcRequest;
@@ -248,8 +249,9 @@ public class DaRPCClient {
 		ClientThread[] benchmarkTask = new ClientThread[threadCount];
 		
 		RdmaRpcProtocol rpcProtocol = new RdmaRpcProtocol();
+		DaRPCMemPool memPool = new DaRPCMemPool("", 0, -1, -1, -1);
 		System.out.println("starting.. threads " + threadCount + ", connections " + connections + ", server " + ipAddress + ", recvQueue " + recvQueue + ", sendQueue" + sendQueue + ", batchSize " + batchSize + ", mode " + mode);
-		DaRPCClientGroup<RdmaRpcRequest, RdmaRpcResponse> group = DaRPCClientGroup.createClientGroup(rpcProtocol, 100, maxinline, recvQueue, sendQueue);
+		DaRPCClientGroup<RdmaRpcRequest, RdmaRpcResponse> group = DaRPCClientGroup.createClientGroup(rpcProtocol, memPool, 100, maxinline, recvQueue, sendQueue);
 		URI uri = URI.create("rdma://" + ipAddress + ":" + 1919);
 		int k = 0;
 		for (int i = 0; i < rpcConnections.length; i++){
